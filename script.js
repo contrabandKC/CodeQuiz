1b37d78faea760b9a6b47fbdc449b95f9db2f28a
erikmarquez <erikmar3@gmail.com>
2020-07-05 01:33:07 -0500

    Added Quiz Function

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-var timerEl = document.getElementById("timer")
+var timerEl = document.querySelector("#timer")
 var buttonEl = document.querySelector("#startButton")
+var startEl = document.querySelector("#start")
+var quizEl = document.querySelector("#quiz")
 
 
+var questionNumber = 0
 
 
-
-
-
-
+var questions = [{
+    question: "question 1",
+    answer: ["answer1","answer 2", "answer 3", "Answer 4" ],
+    correct:1,
+},
+{
+    question: "question 1",
+    answer: ["answer1","answer 2", "answer 3", "Answer 4" ],
+    correct:1,
+}]
 
 function time(){
-    var time = 100
+
+    var timer = 100
 
     var timerInterval = setInterval(function() {
         timer--
         timerEl.textContent = "Time: " + timer
 
     }, 1000)
+
+
 }
 
 // time()
@@ -25,7 +37,11 @@ function time(){
 
 buttonEl.addEventListener("click", function(){
 
+    startEl.setAttribute("style", "display: none;")
+    // console.log(questions[1].question)
 
+    
+   
     time()
 
 
