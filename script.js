e74c935ec9ea1caa8d7d74be92cedebd3b0fda05
erikmarquez <erikmar3@gmail.com>
2020-07-06 01:15:28 -0500

    Added all html Elements

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var timerEl = document.querySelector("#timer")
 var buttonEl = document.querySelector("#startButton")
 var startEl = document.querySelector("#start")
 var quizEl = document.querySelector("#quiz")
-
+var answerEl = document.querySelector("#answers")
 
 var questionNumber = 0
-
+var question = ''
+var answer = ''
+var correct = ''
 
 var questions = [{
     question: "question 1",
@@ -40,7 +42,40 @@ buttonEl.addEventListener("click", function(){
     startEl.setAttribute("style", "display: none;")
     // console.log(questions[1].question)
 
+    var questionEl = document.createElement("div")
+    // var answerEl = document.createElement("div")
+
+    questionEl.textContent = questions[questionNumber].question
+    // answerEl.textContent = questions[questionNumber].answer
+
+    quizEl.appendChild(questionEl)
+
+    questions[questionNumber].answer.forEach(function(element, i) {
+        
     
+        var answers = document.createElement("button")
+        answers.textContent =element
+        // console.log(i)
+
+        answers.setAttribute("data-index", i)
+        answerEl.appendChild(answers)
+
+    });
+    
+    answerEl.addEventListener("click", function (event) {
+        var element = event.target
+        
+        if(element.matches("button")){
+            var index = element.getAttribute("data-index")
+
+            console.log(index)
+
+            if(index == questions[questionNumber].correct){
+                console.log("correct")
+            }
+
+        }
+    })
    
     time()
 
